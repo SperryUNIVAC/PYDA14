a9b744cd1ac3393f6361807966379386742068be
SperryUNIVAC <achtung050@gmail.com>
2020-09-27 18:03:00 +0300

    bugfix

--- Lecture_04/main.py
+++ Lecture_04/main.py
@@ -1,3 +1,20 @@
+'''
+багфиксы:
+1. Добавление документа - исправлено перестановкой действий, теперь скрипт сначала
+проверяет, создана ли полка и если нет, ничего никуда не добавляет.
+2. Удаление документа - там вообще был ад кошмара, я эту функцию, вроде,
+трезвый писал, но что-то нагородил невменяемого, все переписал.
+Страннее всего, что я как-то пролюбил этот момент, я же их все два раза тестил, по идее,
+один раз голую функцию и второй раз, когда все уже собрал. Ну второй раз возможно я ее и забыл,
+но в первый-то оно должно было работать со всеми параметрами возможными.. В общем очень странно, как
+я это упустил.
+3. Перемещение несуществующего документа - добавил обработчик.
+4. Немного изменил information_func(len_list), добавил ей параметр длины списка,
+иначе некорректно обрабатывала выдачу после измененной длины списка после удаления документа
+5. Из get_key_func выкинул print, там забавные побочные эффекты возникали в паре мест,
+когда она печатала там, где не надо
+'''
+
 documents_list = [
 {'type': 'passport', 'number': '2207 876234', 'name': 'Василий Гупкин'},
 {'type': 'invoice', 'number': '11-2', 'name': 'Геннадий Покемонов'},
@@ -19,18 +36,16 @@ def get_key_func(val, directory):
     """
     for key, value in directory.items():
         if val in value:
-           return key
-    else:
-        print('Документ не найден в базе.')
+           return key       
 
 # и функция печати полной информации
 # нужна и сама по себе и встроена в кучу функций ниже
-def information_func():
+def information_func(len_list):
     """
     получение информации и печать
     """
     print('Текущий список документов: ')
-    for i in range(0,len(documents_list)):
+    for i in range(0,len_list):
         print('№: ', documents_list[i]['number'],' тип: ', documents_list[i]['type'],
                ' владелец: ', documents_list[i]['name'], ' полка хранения: ',
                get_key_func(documents_list[i]['number'], directories_dict))
@@ -49,20 +64,23 @@ def relocated_func():
     # проверка на существование документа и одновременно вытаскивание ключа
     else:
         key = get_key_func(number_doc, directories_dict)
+        if not key:
+            print('Документ не существует!')
+        else:
     # удаление ключа
-        delete_list = list(directories_dict[key])
-        delete_list.remove(number_doc)
-        directories_dict.update({key: delete_list})
+            delete_list = list(directories_dict[key])
+            delete_list.remove(number_doc)
+            directories_dict.update({key: delete_list})
     # добавление ключа
-        insert_list = list(directories_dict[number_dict])
-        insert_list.append(number_doc)
-        directories_dict.update({number_dict: insert_list})
-        print('Документ перемещен.')
-        information_func()
+            insert_list = list(directories_dict[number_dict])
+            insert_list.append(number_doc)
+            directories_dict.update({number_dict: insert_list})
+            print('Документ перемещен.')
+            information_func()
 
 def create_new_doc_func():
     """
-    перемещение документа
+    добавление документа
     """
     # создаем новую пустую строку под словарь documents
     documents_list_dict = {}
@@ -75,14 +93,14 @@ def create_new_doc_func():
     documents_list_dict.setdefault('type', type_doc)
     documents_list_dict.setdefault('number', number_doc)
     documents_list_dict.setdefault('name', name_doc)   
-    # добавление нового словаря в список documents
-    documents_list.append(documents_list_dict)
     # прописать документ в directories    
     if dir_doc in directories_dict.keys():
     # если ключ есть, то мы добавляем лист в этот ключ
         directories_dict.setdefault(dir_doc, []).append(number_doc)
+        # добавление нового словаря в список documents
+        documents_list.append(documents_list_dict)
         print('Документ добавлен.')
-        information_func()
+        information_func(len(documents_list))
     else:
         print('Такой полки не существует. Текущий перечень полок: ',
                  *directories_dict, ' Добавьте полку командой as.')
@@ -91,26 +109,20 @@ def del_doc_func():
     """
     удаление документа
     """
-    number_doc = input('Введите номер документа: ')
-    # прогоняем все по-порядку и проверяем, входит ли в вытащенный словарь наше значение
+    number_doc = input('Введите номер документа: ')    
     for i in range(0, len(documents_list)):
         if number_doc in documents_list[i].values():
-    # удаляем запись в документах 
             del documents_list[i]
-    # удаляем запись в папках
             key = get_key_func(number_doc, directories_dict)
             delete_num = list(directories_dict[key])
             delete_num.remove(number_doc)
             directories_dict.update({key: delete_num})
-            print('Документ удален.')
-            information_func()
-    # что бы проверки не понесло дальше
             break
-        else:
-            print('Документ не найден в базе.')
-            information_func()
-    # что бы цикл не понесло дальше
-        break
+    else:
+        print('Документ не найден в базе.')
+        information_func(len(documents_list))
+    print('Документ удален.')
+    information_func(len(documents_list))
 
 def create_new_dir_func():
     """
@@ -123,7 +135,7 @@ def create_new_dir_func():
         directories_dict.update({key: []})
         print('Полка добавлена. Текущий перечень полок: ', *directories_dict)
 
-def delete_new_dir_func():
+def delete_dir_func():
     """
     удаление полки
     """ 
@@ -156,6 +168,8 @@ def find_directory_func():
     number_doc = input('Введите номер документа: ')
     if get_key_func(number_doc, directories_dict):
         print('Полка хранения: ', get_key_func(number_doc, directories_dict))
+    else:
+        print('Документ не найден в базе.')
 
 def main():
     print('Добро пожаловать в скрипт Автоматический секретарь 1.0')
@@ -166,11 +180,11 @@ def main():
         elif command == 's':
             find_directory_func()
         elif command == 'l':
-            information_func()
+            information_func(len(documents_list))
         elif command == 'as':
             create_new_dir_func()
         elif command == 'ds':
-            delete_new_dir_func()
+            delete_dir_func()
         elif command == 'ad':
             create_new_doc_func()
         elif command == 'd':
