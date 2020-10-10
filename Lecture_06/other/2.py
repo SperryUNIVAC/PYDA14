34b82ec41ce2bd1b3eb4df398a7d33f76335b114
SperryUNIVAC <achtung050@gmail.com>
2020-10-10 09:20:43 +0300

    L_06_update

--- Lecture_06/other/2.py
+++ Lecture_06/other/2.py
@@ -4,16 +4,13 @@ some_string = '''Напишите функцию функцию, которая 
 удалять все все все все последовательные повторы слов
 из из из из заданной строки строки при помощи регулярных выражений'''
 
-# здесь у меня непонятный облом. В сервисе regex101 прогон
-# этого регекспа несколько раз приводит строку к нормальному виду, как
-# заказывали, но здесь, почему-то этого не происходит
-
 def duplicate_replacer(some_string):
-    duplicate = r'(\w+) \1[^\w]'
+    duplicate = r'(\w+)(\s\1)+'
     replace = r'\1'
-    for i in range(0, len(some_string.split())):
-        some_string = re.sub(duplicate, replace, some_string)
+    some_string = re.sub(duplicate, replace, some_string)
     return some_string
 
 print(duplicate_replacer(some_string))
+
+
 
