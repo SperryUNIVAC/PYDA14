9090dcf1dccf921021a40f873c1b6b926d0731be
SperryUNIVAC <achtung050@gmail.com>
2021-03-16 18:07:04 +0300

    2_bugfix

---
+++ SQL/3/2_bugfix.sql
@@ -0,0 +1,72 @@
+/* классический метод. Меня дико смутило что там всего 2 магазина и 
+ * я чето очень протупил и не вдуплил - а из чего там вообще выбирать */
+select 
+	customer.store_id,
+	count(customer.store_id) as customer_count
+from "dvd-rental".customer
+group by customer.store_id
+having count(customer.store_id) > 300;
+
+/*стало интересно понял ли я логику и я написал это же через подзапросы*/
+select 
+	customer_count
+from (select 
+		customer.store_id,
+		count(customer.store_id) as customer_count
+	from "dvd-rental".customer
+	group by customer.store_id)
+as cust_coun
+where cust_coun.customer_count > 300;
+
+/*уфф еле вдуплил как последовательно джойнить.*/
+select 
+	cs.customer_id,
+	cs.first_name, 
+	cs.last_name,
+	ad.address,
+	ad.district,
+	ct.city,
+	cn.country
+from "dvd-rental".customer as cs
+join "dvd-rental".address as ad 
+	on cs.address_id = ad.address_id	
+join "dvd-rental".city as ct 
+	on ad.city_id = ct.city_id
+join "dvd-rental".country as cn
+	on ct.country_id = cn.country_id;	
+	
+/*со следующими у меня возникла проблема
+ * я понятия не имею как вобще работает логика операторов в SQL ((
+ * в нормальном языке программирования я создаю переменную 1 даю ей имя А
+ * создаю переменную 2 даю ей имя Б
+ * создаю переменную А+Б даю ей имя С
+ * я не могу понять подобное применимо к SQL
+ * вот я создал джойнами таблицу, прекрасно
+ * как мне теперь передать результат этой работы в функцию count??
+ * ну я могу извратится сделать это через подзапрос, но он выдал мне батарею ошибок и я никак не могу врубится 
+ * в правильный синтаксис этого подзапроса
+ * 
+ * вообще это какой-то извращенный гибрид стекового и функционального языка
+ * я в его парадигму не могу врубится поэтому я даже не понимаю 
+ * в какой последовательности работают эти несчастные операторы
+ * ну базовая конструкция очевидна 
+ * ВЫБРАТЬ фигню ИЗ другой фигни ГДЕ фигня больше 5 ПОКАЗАТЬ 3 последних фигни ПО УБЫВЫАНИЮ фигней
+ * а вот как это склетить с второй такой же логической цепочкой - я реально в саму идею врубится не могу
+ * */
+
+
+select 
+	cs.store_id,
+	sf.first_name, 
+	sf.last_name,
+	ct.city
+from "dvd-rental".customer as cs
+join "dvd-rental".store as st 
+	on cs.store_id = st.store_id	
+join "dvd-rental".staff as sf
+	on	cs.store_id = sf.store_id
+join "dvd-rental".address as ad 
+	on sf.address_id = ad.address_id	
+join "dvd-rental".city as ct 
+	on ad.city_id = ct.city_id
+
